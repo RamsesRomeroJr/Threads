5b8661eb1008a65cd7b8e42d4f45911ae9523995
Ramses Romero <ramses.romero.jr@gmail.com>
2024-01-26 17:11:14 -0500

    created structure to display thread in home page, created ThreadCard component, added thread action to query for thread and its 'children' aka comments

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,35 @@ export async function createThread({text, author, communityId, path} : Params){
     }
 
 };
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20){
+    connectToDB();
+
+    //Calculate the number of posts to skip depending on page we're on
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    //Fetch the posts that have no parents (top-level threads... not comments)
+    const postsQuery = Thread.find({parentId: { $in: [null, undefined]}})
+        .sort({createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User})
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+        // total post count, for total num of pages
+        const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined]} })
+
+        const posts = await postsQuery.exec();
+
+        const isNext = totalPostsCount > skipAmount + posts.length;
+
+        return { posts, isNext}
+
+}
