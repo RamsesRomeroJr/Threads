01712e5a8f92409e4a25f4aa962da72d1d8751b9
Ramses Romero <ramses.romero.jr@gmail.com>
2024-01-31 11:48:32 -0500

    created search page, new component UserCard that shows user image name and username, created new user action to fetch users from database. Created new pages for activity and communities

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
     userId: string,
@@ -85,5 +86,57 @@ export async function fetchUserPosts(userId: string){
         return threads;
     } catch (error: any) {
         throw new Error(`Failed to fetch user posts: ${error.message}`)
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc"
+ } : {
+    userId: string;
+    searchString? : string;
+    pageNumber? : number;
+    pageSize? : number;
+    sortBy? : SortOrder
+ }){
+    try {
+        connectToDB();
+
+        //calculate the number of users to skip based on page number and page size
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query: FilterQuery<typeof User> = {
+            id: {$ne: userId } //$ne: userId means that the id does not equeal our users Id
+        }
+
+        if(searchString.trim() !== ''){
+            query.$or = [
+                { userName: {$regex: regex } },
+                {name: {$regex: regex } }
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy};
+
+        const usersQuery = User.find(query)
+        .sort(sortOptions)
+        .skip(skipAmount)
+        .limit(pageSize)
+
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec()
+
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return { users, isNext };
+
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users: ${error.message}`)
     }
 }
