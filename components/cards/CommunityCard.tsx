4ed9b286f4100e4719918c0ee6208ea8bd2eaaad
Ramses Romero <ramses.romero.jr@gmail.com>
2024-02-16 12:50:28 -0500

    fixed communities page not loading, added check if communities has an image

--- components/cards/CommunityCard.tsx
+++ components/cards/CommunityCard.tsx
@@ -19,12 +19,15 @@ function CommunityCard({ id, name, username, imgUrl, bio, members }: Props) {
     <article className='community-card'>
       <div className='flex flex-wrap items-center gap-3'>
         <Link href={`/communities/${id}`} className='relative h-12 w-12'>
+          {imgUrl ?
+
           <Image
             src={imgUrl}
             alt='community_logo'
             fill
             className='rounded-full object-cover'
-          />
+          /> : <></>
+        }
         </Link>
 
         <div>
